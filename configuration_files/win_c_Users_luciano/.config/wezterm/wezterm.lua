eee113df5504e06900358ccc739c12ebadb5494e
bestia.dev <info@bestia.dev>
2024-03-29 12:40:34 -0500

    2024-03-29

--- configuration_files/win_c_Users_luciano/.config/wezterm/wezterm.lua
+++ configuration_files/win_c_Users_luciano/.config/wezterm/wezterm.lua
@@ -15,8 +15,8 @@ end
 
 -- This is where you actually apply your config choices
 
--- use the Debian bash terminal as default
-config.default_domain = 'WSL:Debian'
+-- never prompt for close confirmation
+config.window_close_confirmation = 'NeverPrompt'
 
 config.keys = {
     -- Turn off the default ctrl+v "input the next character literally",
@@ -36,50 +36,17 @@ config.mouse_bindings = {
 -- launch menu with WSL:Debian and git-bash
  config.launch_menu = {
       {
-         label = "WSL:Debian",
-         args = { "wsl.exe" },
-         domain = { DomainName = "WSL:Debian" },
-        cwd = '/home/',
-      },
-      {
          label = "git-bash",
          args = { "C:\\Program Files\\Git\\bin\\bash.exe", "-l" },
          domain = { DomainName = "local" },
       },
-   }
-
--- on start open 2 windows side-by-side for WSL:Debian and git-bash
-wezterm.on('gui-startup', 
-  function()
-    local win_1_tab_1, win_1_pane_1, win_1 = mux.spawn_window { 
-      -- workspace = 'WSL-Debian',
-      position = {
-          x = 0,
-          y = 0,
-        },
-      width = 72, height = 37,
-      domain = { DomainName = 'WSL:Debian' }
+      {
+         label = "WSL:Debian",
+         args = { "wsl.exe" },
+         domain = { DomainName = "WSL:Debian" },
+        cwd = '/home/',
       }
-
-    local win2_tab_1, win_2_pane_1, win_2 = mux.spawn_window { 
-      -- workspace = 'git-bash' ,
-      position = {
-          x = 901,
-          y = 0,
-        },
-      width = 72, height = 37, 
-      domain = { DomainName = 'local' }, 
-      args = { 'C:\\Program Files\\Git\\bin\\bash.exe' } 
-    }
-
-    win_1:set_title 'WSL:Debian'
-    win_2:set_title 'git-bash'
-
-    win_1_tab_1:set_title 'WSL:Debian'
-    win2_tab_1:set_title 'git-bash'
-
-  end
-)
+   }
 
 -- and finally, return the configuration to wezterm
 return config
